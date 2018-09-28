a8a9e33aa023c5bca219e0f1eff07e0e9ea2bbe1
joymeng <scimence@126.com>
2018-09-28 08:51:33 +0800

    2018_09_28_08.49

---
+++ 生成MD5.bat
@@ -0,0 +1,28 @@
+
+# 
+set PrjName=ChannelConfig
+set DirName=配置文件
+#
+
+set MD5="%~dp0tools\getMD5.exe"
+set MD5_TXT="%~dp0tools\MD5.txt"
+set MD5_updateFiles="%~dp0tools\updateFiles.txt"
+set ContentReplace="%~dp0tools\ContentReplace.exe"
+
+set channelPackages="%~dp0%DirName%"
+set updateFiles="%~dp0MD5.txt"
+set APK_Base="%~dp0\"
+
+if exist %updateFiles% del %updateFiles%
+
+%MD5% %channelPackages%
+%ContentReplace% %MD5_TXT% "%APK_Base%" ""
+%ContentReplace% %MD5_TXT% \ /
+%ContentReplace% %MD5_TXT% "[HeadAppend]" "服务器资源目录(https://raw.githubusercontent.com/joymeng/%PrjName%/master/%DirName%/)\r\n本地资源目录(%APK_Base%)\r\n\r\n待更新文件(\r\n"
+%ContentReplace% %MD5_TXT% "[TailAppend]" "\r\n\r\n)待更新文件\r\n"
+if exist %MD5_updateFiles% del %MD5_updateFiles%
+
+if exist %MD5_TXT% copy %MD5_TXT% %updateFiles%
+if exist %MD5_TXT% del %MD5_TXT%
+
+exit
